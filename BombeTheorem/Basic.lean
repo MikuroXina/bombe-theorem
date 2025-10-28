84820b67637b94b5e2ff50983390a6b22bb8ae1a
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-10-28 19:33:42 +0900

    Simplify Basic

--- BombeTheorem/Basic.lean
+++ BombeTheorem/Basic.lean
@@ -14,25 +14,33 @@ import Mathlib.Logic.Equiv.Defs
 
 namespace Bombe
 
-variable (Cell : Type)
+variable (Cell : Type) (R : Finset Cell) (n : Nat)
 
-/-- The proposition that there are `n` bombs. `R` is a region of cells, `B` is a bomb assignment. -/
-def bombs_in (R : Finset Cell) (B : Finset Cell) (n : Nat) : Prop :=
-  B ⊆ R ∧ ∃ f : B → Fin n, Function.Bijective f
+/-- The proposition that there are `b` bombs in a region of cells `R`. -/
+def bombs_in (R : Finset Cell) (b : Nat) : Prop :=
+  ∃ f : R → Fin b, Function.Bijective f
 
 -- Hints
 
-def HintConstraint := Nat → Prop
-
-def H_exact (n : Nat) : HintConstraint := fun k => k = n
-def H_ge (n : Nat) : HintConstraint := fun k => k ≥ n
-def H_le (n : Nat) : HintConstraint := fun k => k ≤ n
-def H_ne (n : Nat) : HintConstraint := fun k => k ≠ n
-def H_choice01 (n : Nat) : HintConstraint := fun k => k = n ∨ k = n + 1
-def H_choice02 (n : Nat) : HintConstraint := fun k => k = n ∨ k = n + 2
-def H_choice03 (n : Nat) : HintConstraint := fun k => k = n ∨ k = n + 3
-def H_choice012 (n : Nat) : HintConstraint := fun k => k = n ∨ k = n + 1 ∨ k = n + 2
-def H_choice024 (n : Nat) : HintConstraint := fun k => k = n ∨ k = n + 3 ∨ k = n + 4
-def H_mod2 (n : Nat) : HintConstraint := fun k => (k - n) % 2 = 0
+def H_exact : Prop :=
+  ∃ b, bombs_in Cell R b ∧ b = n
+def H_ge : Prop :=
+  ∃ b, bombs_in Cell R b ∧ b ≥ n
+def H_le : Prop :=
+  ∃ b, bombs_in Cell R b ∧ b ≤ n
+def H_ne : Prop :=
+  ∃ b, bombs_in Cell R b ∧ b ≠ n
+def H_choice01 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b = n ∨ b = n + 1)
+def H_choice02 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b = n ∨ b = n + 2)
+def H_choice03 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b = n ∨ b = n + 3)
+def H_choice012 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b = n ∨ b = n + 1 ∨ b = n + 2)
+def H_choice024 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b = n ∨ b = n + 3 ∨ b = n + 4)
+def H_mod2 : Prop :=
+  ∃ b, bombs_in Cell R b ∧ (b - n) % 2 = 0
 
 end Bombe
