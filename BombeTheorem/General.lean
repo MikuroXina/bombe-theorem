5d12d85e735a0a1a3c15f897ee8fba974c3a6e5e
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-10-28 19:53:02 +0900

    Fix errors

--- BombeTheorem/General.lean
+++ BombeTheorem/General.lean
@@ -1,25 +1,28 @@
-import Mathlib.Data.Set.Basic
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Nat.Order.Lemmas
 import Mathlib.Data.Finset.Basic
-import Mathlib.Data.Finset.Card
-import Mathlib.Data.Finset.Defs
-import Mathlib.Data.Fintype.Basic
-import Mathlib.Data.Fintype.Card
-import Mathlib.Data.Fintype.Defs
-import Mathlib.Data.Fintype.EquivFin
-import Mathlib.Data.Fintype.Sets
-import Mathlib.Logic.Equiv.Basic
-import Mathlib.Logic.Equiv.Defs
 import BombeTheorem.Basic
 
-open Set
 open Finset
 open Bombe
 
+variable (Cell : Type)
+
 /-- 0! は 1+ に等しい. -/
-theorem zero_not_eq_is_one_ge (k : Nat) : H_ne 0 k ↔ H_ge 1 k := by
+theorem zero_not_eq_is_one_ge (A : Finset Cell) : H_ne Cell A 0 ↔ H_ge Cell A 1 := by
   unfold H_ne H_ge
   constructor
-  · exact Nat.pos_of_ne_zero
-  · exact Nat.ne_of_gt
+  · intro h
+    rcases h with ⟨b, h_b⟩
+    use b
+    constructor
+    · exact h_b.left
+    · apply Nat.pos_of_ne_zero
+      exact h_b.right
+  · intro h
+    rcases h with ⟨b, h_b⟩
+    use b
+    constructor
+    · exact h_b.left
+    · apply Nat.ne_of_gt
+      exact h_b.right
