0c9d9a584feec5c7e892b2aa3b06851958a0deed
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-10-29 04:08:01 +0900

    Split proposition among areas and hints

--- BombeTheorem/Basic.lean
+++ BombeTheorem/Basic.lean
@@ -20,30 +20,50 @@ abbrev Cell := Nat
 
 variable (R : Finset Cell) (n : Nat)
 
-/-- The proposition that there are `b` bombs in a region of cells `R`. -/
-def bombs_in : Prop := #R = n
+-- Areas
+
+def A_exact : Prop :=
+  #R = n
+def A_ge : Prop :=
+  #R ≥ n
+def A_le : Prop :=
+  #R ≤ n
+def A_ne : Prop :=
+  #R ≠ n
+def A_choice01 : Prop :=
+  (#R = n ∨ #R = n + 1)
+def A_choice02 : Prop :=
+  (#R = n ∨ #R = n + 2)
+def A_choice03 : Prop :=
+  (#R = n ∨ #R = n + 3)
+def A_choice012 : Prop :=
+  (#R = n ∨ #R = n + 1 ∨ #R = n + 2)
+def A_choice024 : Prop :=
+  (#R = n ∨ #R = n + 3 ∨ #R = n + 4)
+def A_mod2 : Prop :=
+  (#R - n) % 2 = 0
 
 -- Hints
 
 def H_exact : Prop :=
-  #R = n
+  ∃ B ⊆ R, #B = n
 def H_ge : Prop :=
-  #R ≥ n
+  ∃ B ⊆ R, #B ≥ n
 def H_le : Prop :=
-  #R ≤ n
+  ∃ B ⊆ R, #B ≤ n
 def H_ne : Prop :=
-  #R ≠ n
+  ∃ B ⊆ R, #B ≠ n
 def H_choice01 : Prop :=
-  (#R = n ∨ #R = n + 1)
+  ∃ B ⊆ R, #B = n ∨ #B = n + 1
 def H_choice02 : Prop :=
-  (#R = n ∨ #R = n + 2)
+  ∃ B ⊆ R, #B = n ∨ #B = n + 2
 def H_choice03 : Prop :=
-  (#R = n ∨ #R = n + 3)
+  ∃ B ⊆ R, #B = n ∨ #B = n + 3
 def H_choice012 : Prop :=
-  (#R = n ∨ #R = n + 1 ∨ #R = n + 2)
+  ∃ B ⊆ R, #B = n ∨ #B = n + 1 ∨ #B = n + 2
 def H_choice024 : Prop :=
-  (#R = n ∨ #R = n + 3 ∨ #R = n + 4)
+  ∃ B ⊆ R, #B = n ∨ #B = n + 2 ∨ #B = n + 4
 def H_mod2 : Prop :=
-  (#R - n) % 2 = 0
+  ∃ B ⊆ R, (#B - n) % 2 = 0
 
 end Bombe
